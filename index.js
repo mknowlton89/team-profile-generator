588cfbc77dcd6ff4b71e5f098931a0ca29c5c462
Michael Knowlton <mknowlton89@gmail.com>
2021-04-12 09:35:20 -0400

    Started chaining the prompt functions together to build out the logic.

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+let whatWouldYouLikeToAdd = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'chooseType',
+            message: 'Would you like to add an Engineer, an Intern, or quit the program?',
+            choices: ['Add an Engineer', 'Add an Intern', 'Quit the Program'],
+        }])
+}
+
+const addEngineer = () => {
+    // inquirer.prompt([
+    //     {
+    //         type: 'list',
+    //         name: 'chooseType',
+    //         message: 'Would you like to add an Engineer, an Intern, or quit the program?',
+    //         choices: ['Add an Engineer', 'Add an Intern', 'Quit the Program'],
+    //     }])
+    console.log("addEngineer was called");
+}
 
 
 
@@ -20,28 +40,19 @@ inquirer
             type: 'input',
             name: 'managerEmail',
             message: "What is the team manager's Email?",
-        }]
-        // {
-        //     type: 'list',
-        //     name: 'chooseType',
-        //     message: 'Would you like to add an Engineer, an Intern, or quit the program?',
-        //     choices: ['Add an Engineer', 'Add an Intern', 'Quit the Program'],
-        // }
-        .then((results) => whatWouldYouLikeToAdd
-            .prompt([
-                {
-                    type: 'list',
-                    name: 'chooseType',
-                    message: 'Would you like to add an Engineer, an Intern, or quit the program?',
-                    choices: ['Add an Engineer', 'Add an Intern', 'Quit the Program'],
-                }])
-            // console.log("Success!")
-            // const badges = project.badges;
-            // project.badgeString = buildBadgeUrls(badges);
-            // const readMeContent = readMeTemplate(project);
+        },
+        {
+            type: 'list',
+            name: 'chooseType',
+            message: 'Would you like to add an Engineer, an Intern, or quit the program?',
+            choices: ['Add an Engineer', 'Add an Intern', 'Quit the Program'],
+        }])
+    .then((answers) => {
+        // IF THEY CHOSE ENGINEER - CALL THE add
+        console.log(answers);
+        if (answers.chooseType === "Add an Engineer") {
+            addEngineer();
+        }
+    })
 
-            // fs.writeFile('README.md', readMeContent, (err) =>
-            //     err ? console.log(err) : console.log('Successfully created README!')
-            // );
-
-            .catch((err) => new Error(err))))
+            // .catch((err) => new Error(err))))
